f958ded0cdb772c04dc3592476e8b84f33edecbc
BatchLion <30330388+BatchLion@users.noreply.github.com>
2019-09-09 19:57:30 +0800

    Add files via upload

---
+++ 基础习题课思考题/常微分方程部分的思考题/常微分方程的基本概念和初等解法/常微分方程-思考题2/常微分方程-思考题v1.mm
@@ -0,0 +1,140 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="&#x5e38;&#x5fae;&#x5206;&#x65b9;&#x7a0b;" FOLDED="false" ID="ID_163487068" CREATED="1559399608810" MODIFIED="1559402398907" STYLE="oval">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="24"/>
+<hook NAME="MapStyle" zoom="0.751">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="6" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="&#x5fae;&#x5206;&#x65b9;&#x7a0b;&#x7684;&#x57fa;&#x672c;&#x6982;&#x5ff5;" POSITION="right" ID="ID_517445802" CREATED="1559399658509" MODIFIED="1559402398905" HGAP_QUANTITY="20.74999979883433 pt" VSHIFT_QUANTITY="5.249999843537812 pt">
+<edge COLOR="#ff0000"/>
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="16"/>
+</node>
+<node TEXT="&#x5fae;&#x5206;&#x65b9;&#x7a0b;&#x7684;&#x521d;&#x7b49;&#x89e3;&#x6cd5;" POSITION="right" ID="ID_1884057012" CREATED="1559399745399" MODIFIED="1559403757140" HGAP_QUANTITY="23.74999970942737 pt" VSHIFT_QUANTITY="19.499999418854735 pt">
+<edge COLOR="#0000ff"/>
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="16"/>
+<node TEXT="\latex {\bf&#x5206;&#x79bb;&#x53d8;&#x91cf;&#x6cd5;} $\frac{\mathrm dy}{\mathrm dx}=f(x)g(y)$" ID="ID_1896892743" CREATED="1559399980306" MODIFIED="1559403757132" HGAP_QUANTITY="23.749999709427364 pt" VSHIFT_QUANTITY="5.999999821186073 pt">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="14"/>
+<node TEXT="\latex {\bf\textcolor{red}{&#x601d;&#x8003;&#x9898;&#xff1a;}}&#x6c42;&#x4e0b;&#x5217;&#x5fae;&#x5206;&#x65b9;&#x7a0b;&#x7684;&#x901a;&#x89e3;&#xff1a; $x^2y&apos;+y=0$." ID="ID_367995534" CREATED="1559403531713" MODIFIED="1559403746426">
+<font SIZE="14"/>
+</node>
+</node>
+<node TEXT="\latex {\bf&#x9f50;&#x6b21;&#x65b9;&#x7a0b;} $\frac{\mathrm dy}{\mathrm dx}=g(\frac yx)$" ID="ID_289682437" CREATED="1559401117780" MODIFIED="1559402005393" HGAP_QUANTITY="19.24999984353781 pt" VSHIFT_QUANTITY="28.499999150633837 pt">
+<font SIZE="14"/>
+</node>
+<node TEXT="\latex {\bf&#x4e00;&#x9636;&#x7ebf;&#x6027;&#x5fae;&#x5206;&#x65b9;&#x7a0b;} $\frac{\mathrm dy}{\mathrm dx}+p(x)y=q(x)$" ID="ID_1942672626" CREATED="1559400016573" MODIFIED="1559403219041" HGAP_QUANTITY="22.249999754130847 pt" VSHIFT_QUANTITY="-24.749999262392542 pt">
+<font SIZE="14"/>
+<node TEXT="\latex &#x901a;&#x89e3; $y=\mathrm e^{-\int p(x)\mathrm dx}(\int q(x)\mathrm e^{\int p(x)\mathrm dx}\mathrm dx+C)$" ID="ID_1117747317" CREATED="1559400648269" MODIFIED="1559403205782" HGAP_QUANTITY="25.999999642372142 pt" VSHIFT_QUANTITY="-9.749999709427364 pt">
+<font SIZE="12"/>
+</node>
+<node TEXT="\latex &#x5e38;&#x6570;&#x53d8;&#x6613;&#x6cd5; $y=C\mathrm e^{-\int p(x)\mathrm dx}\Rightarrow y=C(x)\mathrm e^{-\int p(x)\mathrm dx}" ID="ID_917261345" CREATED="1559400841211" MODIFIED="1559403207649" HGAP_QUANTITY="22.999999731779106 pt" VSHIFT_QUANTITY="3.7499998882412937 pt">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="\latex {\bf&#x4f2f;&#x52aa;&#x5229;&#x65b9;&#x7a0b;}$\frac{\mathrm dy}{\mathrm dx}+p(x)y=q(x)y^n,n\neq0,1$" ID="ID_545349313" CREATED="1559400642456" MODIFIED="1559403221173" HGAP_QUANTITY="26.749999620020397 pt" VSHIFT_QUANTITY="2.2499999329447764 pt">
+<font SIZE="14"/>
+<node TEXT="\latex &#x53d8;&#x91cf;&#x4ee3;&#x6362;&#x6cd5; &#x4ee4;$z=y^{1-n}$" ID="ID_1216818361" CREATED="1559401186445" MODIFIED="1559403190287" HGAP_QUANTITY="22.249999754130847 pt" VSHIFT_QUANTITY="1.4999999552965164 pt">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="12"/>
+</node>
+<node TEXT="\latex &#x5e38;&#x6570;&#x53d8;&#x6613;&#x6cd5;" ID="ID_1861140124" CREATED="1559401321359" MODIFIED="1559403195123" HGAP_QUANTITY="25.249999664723884 pt" VSHIFT_QUANTITY="2.9999999105930355 pt">
+<font NAME="Serif" SIZE="12"/>
+</node>
+</node>
+<node TEXT="\latex {\bf&#x5168;&#x5fae;&#x5206;&#x65b9;&#x7a0b;} $X(x,y)\mathrm dx+Y(x,y)\mathrm dy=0$" ID="ID_1287571478" CREATED="1559401182196" MODIFIED="1559403263258" HGAP_QUANTITY="28.24999957531692 pt" VSHIFT_QUANTITY="25.4999992400408 pt">
+<font SIZE="14"/>
+<node TEXT="\latex &#x5145;&#x8981;&#x6761;&#x4ef6;&#xff1a;&#x5e73;&#x9762;&#x5355;&#x8fde;&#x901a;&#x57df;$D$&#x4e0a;$\frac{\partial Y}{\partial x}=\frac{\partial X}{\partial y}$" ID="ID_1436733778" CREATED="1559401475005" MODIFIED="1559403259630" VSHIFT_QUANTITY="-10.499999687075624 pt">
+<font SIZE="12" BOLD="true"/>
+</node>
+<node TEXT="\latex &#x6c42;&#x6cd5;" ID="ID_1825731623" CREATED="1559401588151" MODIFIED="1559403281367">
+<font SIZE="12"/>
+<node TEXT="\latex &#x53d8;&#x4e0a;&#x9650;&#x79ef;&#x5206;&#x6cd5; $\int_{(x_0,y_0)}^{(x,y)}X\mathrm dx+Y\mathrm dy=C$" ID="ID_560643531" CREATED="1559401716558" MODIFIED="1559403267296" VSHIFT_QUANTITY="-5.249999843537812 pt"/>
+<node TEXT="\latex &#x4e0d;&#x5b9a;&#x79ef;&#x5206;&#x6cd5;" ID="ID_761457957" CREATED="1559401800320" MODIFIED="1559403281365" HGAP_QUANTITY="14.0 pt" VSHIFT_QUANTITY="0.7499999776482582 pt"/>
+<node TEXT="\latex &#x5206;&#x9879;&#x7ec4;&#x5408;&#x51d1;&#x5fae;&#x5206;&#x6cd5;" ID="ID_1285079199" CREATED="1559401861695" MODIFIED="1559403270136" VSHIFT_QUANTITY="5.999999821186071 pt"/>
+</node>
+<node TEXT="\latex &#x79ef;&#x5206;&#x56e0;&#x5b50;" ID="ID_1527860791" CREATED="1559402253157" MODIFIED="1559403263256" HGAP_QUANTITY="17.749999888241295 pt" VSHIFT_QUANTITY="11.249999664723884 pt">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="\latex {\bf&#x53ef;&#x964d;&#x9636;&#x7684;&#x9ad8;&#x9636;&#x5fae;&#x5206;&#x65b9;&#x7a0b;}" ID="ID_1922796580" CREATED="1559402292769" MODIFIED="1559403228980" HGAP_QUANTITY="34.99999937415125 pt" VSHIFT_QUANTITY="11.999999642372142 pt">
+<font SIZE="14"/>
+<node TEXT="\latex \textcircled{1}$y^{(n)}=f(x)$ &#x5219;$n$&#x6b21;&#x79ef;&#x5206;" ID="ID_1052241508" CREATED="1559402407267" MODIFIED="1559402833773"/>
+<node TEXT="\latex \textcircled{2} &#x53f3;&#x7aef;&#x4e0d;&#x663e;&#x542b;$y$&#x7684;&#x4e8c;&#x9636;&#x65b9;&#x7a0b; $y&apos;&apos;=f(x,y&apos;)$, &#x4ee4;$p(x)=y&apos;$" ID="ID_1694798667" CREATED="1559402430831" MODIFIED="1559402996953"/>
+<node TEXT="\latex \textcircled{3} &#x53f3;&#x7aef;&#x4e0d;&#x663e;&#x542b;$x$&#x7684;&#x4e8c;&#x9636;&#x65b9;&#x7a0b; $y&apos;&apos;=f(y,y&apos;)$, &#x4ee4;$y&apos;=p(y), y&apos;&apos;=p&apos;y&apos;=p&apos;p$" ID="ID_35479586" CREATED="1559402848249" MODIFIED="1559403130017"/>
+</node>
+</node>
+<node TEXT="&#x9ad8;&#x9636;&#x7ebf;&#x6027;&#x5fae;&#x5206;&#x65b9;&#x7a0b;&#x89e3;&#x7684;&#x7ed3;&#x6784;" POSITION="right" ID="ID_70668846" CREATED="1559399756549" MODIFIED="1559399866048">
+<edge COLOR="#00ff00"/>
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="16"/>
+</node>
+<node TEXT="&#x9ad8;&#x9636;&#x7ebf;&#x6027;&#x5e38;&#x7cfb;&#x6570;&#x9f50;&#x6b21;&#x5fae;&#x5206;&#x65b9;&#x7a0b;" POSITION="right" ID="ID_1934604970" CREATED="1559399771600" MODIFIED="1559399886776">
+<edge COLOR="#ff00ff"/>
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="16"/>
+</node>
+<node TEXT="&#x7ebf;&#x6027;&#x5e38;&#x7cfb;&#x6570;&#x5fae;&#x5206;&#x65b9;&#x7a0b;&#x7ec4;" POSITION="right" ID="ID_1594311166" CREATED="1559399784204" MODIFIED="1559399890890">
+<edge COLOR="#00ffff"/>
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="16"/>
+</node>
+<node TEXT="&#x7a33;&#x5b9a;&#x6027;&#x521d;&#x6b65;" POSITION="right" ID="ID_712518842" CREATED="1559399803538" MODIFIED="1559399901052">
+<edge COLOR="#7c0000"/>
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="16"/>
+</node>
+</node>
+</map>
