a775593fa199e1b97c44d6527a93e914f9bd48f7
BatchLion <30330388+BatchLion@users.noreply.github.com>
2019-09-10 11:04:34 +0800

    Add files via upload

---
+++ 基础习题课期末复习/Source Codes/20190609/多元函数微分学-方向导数、梯度、泰勒公式.mm
@@ -0,0 +1,180 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="&#x591a;&#x5143;&#x51fd;&#x6570;&#x5fae;&#x5206;&#x5b66;" FOLDED="false" ID="ID_1559863820" CREATED="1560081923662" MODIFIED="1560081990418" STYLE="oval">
+<font SIZE="72"/>
+<hook NAME="MapStyle" zoom="0.123">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="72"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<font SIZE="72"/>
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork" MAX_WIDTH="120.0 cm">
+<font NAME="SansSerif" SIZE="72" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<font SIZE="72"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<font SIZE="72"/>
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="72" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="72" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="72" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+<font SIZE="72"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<font SIZE="72"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<font SIZE="72"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="8" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="&#x591a;&#x5143;&#x8fde;&#x7eed;&#x51fd;&#x6570;" POSITION="right" ID="ID_1034412262" CREATED="1560081945473" MODIFIED="1560081998663">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="&#x504f;&#x5bfc;&#x6570;" POSITION="right" ID="ID_1150579441" CREATED="1560081999252" MODIFIED="1560082059075">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="&#x5168;&#x5fae;&#x5206;" POSITION="right" ID="ID_1918211314" CREATED="1560082060085" MODIFIED="1560082062699">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="&#x590d;&#x5408;&#x51fd;&#x6570;&#x5fae;&#x5206;&#x6cd5;" POSITION="right" ID="ID_323768876" CREATED="1560082063193" MODIFIED="1560082067214">
+<edge COLOR="#ff00ff"/>
+</node>
+<node TEXT="&#x9690;&#x51fd;&#x6570;&#x5fae;&#x5206;&#x6cd5;" POSITION="right" ID="ID_1202194146" CREATED="1560082067767" MODIFIED="1560082072030">
+<edge COLOR="#00ffff"/>
+</node>
+<node TEXT="&#x65b9;&#x5411;&#x5bfc;&#x6570;&#x548c;&#x68af;&#x5ea6;" POSITION="right" ID="ID_540838570" CREATED="1560082072393" MODIFIED="1560082078464">
+<edge COLOR="#7c0000"/>
+<node TEXT="&#x591a;&#x5143;&#x51fd;&#x6570;&#x7684;&#x65b9;&#x5411;&#x5bfc;&#x6570;" ID="ID_1958940906" CREATED="1560082086395" MODIFIED="1560084904824">
+<node TEXT="&#x65b9;&#x5411;&#x5bfc;&#x6570;&#x7684;&#x6982;&#x5ff5;" ID="ID_325595977" CREATED="1560084906881" MODIFIED="1560090342697">
+<node TEXT="\latex $\textit\mathbf l=(\cos\alpha,\sin\alpha), \frac{\partial f(a,b)}{\partial\textit\mathbf l}=\lim\limits_{t\rightarrow0}\frac{f(a+t\cos\alpha,b+t\sin\alpha)-f(a,b)}t$" ID="ID_1155020494" CREATED="1560090007390" MODIFIED="1560091691948"/>
+<node TEXT="\latex $g(t)=f(a+t\cos\alpha,b+t\sin\alpha), \frac{\partial f(a,b)}{\partial\textit\mathbf l}=g&apos;(0)$" ID="ID_435944805" CREATED="1560090005642" MODIFIED="1560091720969"/>
+<node TEXT="&#x504f;&#x5bfc;&#x6570;&#x662f;&#x7279;&#x6b8a;&#x7684;&#x65b9;&#x5411;&#x5bfc;&#x6570;" ID="ID_1555696052" CREATED="1560090960925" MODIFIED="1560090969941"/>
+</node>
+<node TEXT="&#x65b9;&#x5411;&#x5bfc;&#x6570;&#x7684;&#x8ba1;&#x7b97;" ID="ID_16363238" CREATED="1560084916314" MODIFIED="1560091138765">
+<node TEXT="\latex $f(x,y)$&#x5728;&#x70b9;$(a,b)$&#x5904;&#x53ef;&#x5fae;$\Rightarrow\frac{\partial f(x,y)}{\partial\textit\mathbf l}=\frac{\partial f}{\partial x}\cos\alpha+\frac{\partial f}{\partial y}\sin\alpha$" ID="ID_351283346" CREATED="1560091008174" MODIFIED="1560091745057" HGAP_QUANTITY="30.499999508261695 pt" VSHIFT_QUANTITY="33.74999899417165 pt">
+<node TEXT="\latex $\textit\mathbf l=(\cos\alpha,\sin\alpha)$ &#x662f;&#x5355;&#x4f4d;&#x5411;&#x91cf;" ID="ID_1083092292" CREATED="1560091180747" MODIFIED="1560091759472" HGAP_QUANTITY="-1128.2499659582982 pt" VSHIFT_QUANTITY="140.2499958202244 pt"/>
+</node>
+</node>
+</node>
+<node TEXT="&#x68af;&#x5ea6;" ID="ID_224550918" CREATED="1560082090285" MODIFIED="1560082092358">
+<node TEXT="&#x68af;&#x5ea6;&#x5411;&#x91cf;&#x7684;&#x6982;&#x5ff5;" ID="ID_163238420" CREATED="1560087044051" MODIFIED="1560093969915">
+<node TEXT="\latex $\text{grad} f(a,b)=\frac{\partial f(a,b)}{\partial\textit\mathbf l_0}\textit\mathbf l_0$, &#x5176;&#x4e2d;$\textit\mathbf l_0$&#x6ee1;&#x8db3;$\frac{\partial f(a,b)}{\partial\textit\mathbf l_0}=\max\limits_{\|\textit\mathbf l\|=1}\{\frac{\partial f(a,b)}{\partial\textit\mathbf l}\}$" ID="ID_782737603" CREATED="1560091277144" MODIFIED="1560093969910" HGAP_QUANTITY="-434.49998663365875 pt" VSHIFT_QUANTITY="174.74999479204433 pt">
+<node TEXT="&#x68af;&#x5ea6;&#x5411;&#x91cf;&#x7684;&#x65b9;&#x5411;&#xff1a;&#x53d6;&#x5230;&#x6700;&#x5927;&#x7684;&#x65b9;&#x5411;&#x5bfc;&#x6570;" ID="ID_587745266" CREATED="1560091514120" MODIFIED="1560092024357"/>
+<node TEXT="&#x68af;&#x5ea6;&#x5411;&#x91cf;&#x7684;&#x957f;&#x5ea6;&#xff1a;&#x65b9;&#x5411;&#x5bfc;&#x6570;&#x7684;&#x6700;&#x5927;&#x503c;" ID="ID_1783175852" CREATED="1560092024841" MODIFIED="1560092038573"/>
+</node>
+</node>
+<node TEXT="&#x68af;&#x5ea6;&#x5411;&#x91cf;&#x7684;&#x8ba1;&#x7b97;" ID="ID_1754184850" CREATED="1560087049851" MODIFIED="1560093982842">
+<node TEXT="\latex $f(x,y)$&#x5728;&#x70b9;$(a,b)$&#x5904;&#x53ef;&#x5fae;$\Rightarrow\text{grad}f(a,b)=(\frac{\partial f(a,b)}{\partial x},\frac{\partial f(a,b)}{\partial y})$" ID="ID_1844517098" CREATED="1560092169482" MODIFIED="1560093982839" HGAP_QUANTITY="-441.9999864101414 pt" VSHIFT_QUANTITY="154.49999539554133 pt">
+<node TEXT="\latex $f(x,y)$&#x5728;&#x70b9;$(a,b)$&#x5904;&#x53ef;&#x5fae;$\Rightarrow\frac{\partial f(a,b)}{\partial\textit\mathbf l}=\text{grad}f(a,b)\mathbf\cdot\textit\mathbf l$" ID="ID_604872739" CREATED="1560092276236" MODIFIED="1560092368546"/>
+<node TEXT="&#x68af;&#x5ea6;&#x5411;&#x91cf;&#x4e0e;&#x7b49;&#x503c;&#x7ebf;&#x5782;&#x76f4;" ID="ID_1703046991" CREATED="1560092374953" MODIFIED="1560092410110"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="&#x6620;&#x5c04;&#x53ca;&#x5176;&#x5fae;&#x5206;" POSITION="right" ID="ID_980934807" CREATED="1560083297636" MODIFIED="1560088475642">
+<edge COLOR="#007c00"/>
+<node TEXT="\latex $\mathbb R^n\rightarrow\mathbb R^m$&#x7684;&#x6620;&#x5c04;" ID="ID_1374441695" CREATED="1560088477557" MODIFIED="1560088520240">
+<node TEXT="&#x5b9a;&#x4e49;" ID="ID_939865501" CREATED="1560088530221" MODIFIED="1560088537020"/>
+<node TEXT="&#x8fde;&#x7eed;&#x6620;&#x5c04;" ID="ID_465781698" CREATED="1560088539338" MODIFIED="1560088542161"/>
+</node>
+<node TEXT="&#x6620;&#x5c04;&#x7684;&#x5fae;&#x5206;" ID="ID_110658716" CREATED="1560088520823" MODIFIED="1560088554824">
+<node TEXT="&#x6982;&#x5ff5;" ID="ID_1608905557" CREATED="1560088561082" MODIFIED="1560088562916"/>
+<node TEXT="&#x6620;&#x5c04;&#x7684;Jacobi&#x77e9;&#x9635;" ID="ID_26678569" CREATED="1560088563139" MODIFIED="1560088592657"/>
+</node>
+<node TEXT="&#x590d;&#x5408;&#x6620;&#x5c04;&#x7684;&#x5fae;&#x5206;&#x6cd5;" FOLDED="true" ID="ID_617554941" CREATED="1560088555163" MODIFIED="1560088615484">
+<node TEXT="&#x590d;&#x5408;&#x6620;&#x5c04;&#x7684;&#x6982;&#x5ff5;" ID="ID_703560464" CREATED="1560088616376" MODIFIED="1560088620029"/>
+<node TEXT="&#x590d;&#x5408;&#x6620;&#x5c04;&#x7684;&#x5fae;&#x5206;&#x6cd5;&#xff08;&#x94fe;&#x5bfc;&#x6cd5;&#x5219;&#xff09;" ID="ID_386553207" CREATED="1560088620173" MODIFIED="1560088680350">
+<node TEXT="&#x9006;&#x6620;&#x5c04;&#x7684;&#x5fae;&#x5206;&#x6cd5;" ID="ID_1240987303" CREATED="1560088667773" MODIFIED="1560088680344" HGAP_QUANTITY="17.749999888241295 pt" VSHIFT_QUANTITY="32.99999901652339 pt"/>
+</node>
+</node>
+<node TEXT="\latex &#x4e00;&#x5143;&#x5411;&#x91cf;&#x503c;&#x51fd;&#x6570;($\mathbb R^1\rightarrow\mathbb R^m$&#x7684;&#x6620;&#x5c04;)&#x7684;&#x5bfc;&#x6570;&#x4e0e;&#x79ef;&#x5206;" ID="ID_590875515" CREATED="1560088699119" MODIFIED="1560088759880">
+<node TEXT="&#x4e00;&#x5143;&#x5411;&#x91cf;&#x503c;&#x51fd;&#x6570;&#x7684;&#x6982;&#x5ff5;" ID="ID_462183887" CREATED="1560088764489" MODIFIED="1560088777995">
+<node TEXT="\latex $\textit\mathbf r=\textit\mathbf f(t), t\in\mathbb R, \textit\mathbf r\in\mathbb R^n$" ID="ID_804245927" CREATED="1560092530774" MODIFIED="1560092604093"/>
+</node>
+<node TEXT="&#x4e00;&#x5143;&#x5411;&#x91cf;&#x503c;&#x51fd;&#x6570;&#x7684;&#x5bfc;&#x6570;" ID="ID_1046789685" CREATED="1560088778591" MODIFIED="1560088787105">
+<node TEXT="\latex $\textit\mathbf f&apos;(t)=\begin{pmatrix}x&apos;(t)\\ y&apos;(t) \\ z&apos;(t)\end{pmatrix}$" ID="ID_429355813" CREATED="1560092657856" MODIFIED="1560092791840"/>
+<node TEXT="\latex $\mathrm d\textit\mathbf f&apos;(t)=\begin{pmatrix}x&apos;(t)\mathrm dt\\ y&apos;(t)\mathrm dt\\ z&apos;(t)\mathrm dt\end{pmatrix}$" ID="ID_1733142635" CREATED="1560092801282" MODIFIED="1560092856677"/>
+</node>
+<node TEXT="&#x6c42;&#x5bfc;&#x6cd5;&#x5219;" ID="ID_1817688101" CREATED="1560088787438" MODIFIED="1560088809677">
+<node TEXT="\latex $(c\textit\mathbf u(t))&apos;=c\textit\mathbf u&apos;(t)$" ID="ID_1933734078" CREATED="1560092943174" MODIFIED="1560093051043"/>
+<node TEXT="\latex $(\textit\mathbf u(t)+\textit\mathbf v(t))&apos;=\textit\mathbf u&apos;(t)+\textit\mathbf v&apos;(t)$" ID="ID_1592142074" CREATED="1560092947378" MODIFIED="1560093041722"/>
+<node TEXT="\latex $(\lambda(t)\textit\mathbf u(t))&apos;=\lambda&apos;(t)\textit\mathbf u(t)+\lambda(t)\textit\mathbf u&apos;(t)$" ID="ID_819958148" CREATED="1560092950880" MODIFIED="1560093167424"/>
+<node TEXT="\latex $(\textit\mathbf u(t)\mathbf\cdot\textit\mathbf v(t))&apos;=\textit\mathbf u&apos;(t)\mathbf\cdot\textit\mathbf v(t)+\textit\mathbf u(t)\mathbf\cdot\textit\mathbf v&apos;(t)$" ID="ID_1834624270" CREATED="1560092952427" MODIFIED="1560093248212"/>
+<node TEXT="\latex $(\textit\mathbf u(t)\times\textit\mathbf v(t))&apos;=\textit\mathbf u&apos;(t)\times\textit\mathbf v(t)+\textit\mathbf u(t)\times\textit\mathbf v&apos;(t)$" ID="ID_1905149813" CREATED="1560092954705" MODIFIED="1560093276271"/>
+</node>
+<node TEXT="&#x5411;&#x91cf;&#x503c;&#x51fd;&#x6570;&#x7684;&#x79ef;&#x5206;" ID="ID_484688491" CREATED="1560088804362" MODIFIED="1560088819695">
+<node TEXT="\latex $\int_a^b\textit\mathbf f(t)\mathrm dt=\begin{pmatrix}\int_a^bx(t)\mathrm dt\\ \int_a^by(t)\mathrm dt\\ \int_a^bz(t)\mathrm dt\end{pmatrix}$" ID="ID_1457303311" CREATED="1560093293734" MODIFIED="1560093490838"/>
+</node>
+</node>
+</node>
+<node TEXT="&#x591a;&#x5143;&#x51fd;&#x6570;&#x7684;&#x6cf0;&#x52d2;&#x516c;&#x5f0f;" POSITION="right" ID="ID_1990076235" CREATED="1560082078860" MODIFIED="1560088833047">
+<edge COLOR="#00007c"/>
+<node TEXT="&#x4e8c;&#x5143;&#x51fd;&#x6570;&#x7684;n&#x9636;&#x5e26;&#x6709; Peano&#x578b;&#x4f59;&#x9879;&#x7684;&#x6cf0;&#x52d2;&#x516c;&#x5f0f;" ID="ID_1990593677" CREATED="1560088847503" MODIFIED="1560094036163">
+<node TEXT="\latex $f(x,y)=f(a,b)\\+[\frac\partial{\partial x}(x-a)+\frac\partial{\partial y}(y-b)]f(a,b)\\+\frac12[\frac\partial{\partial x}(x-a)+\frac\partial{\partial y}(y-b)]^2f(a,b)\\+\cdots\\+\frac1{n!}[\frac\partial{\partial x}(x-a)+\frac\partial{\partial y}(y-b)]^nf(a,b)\\+o((\sqrt{(x-a)^2+(y-b)^2})^n)$" ID="ID_1623400583" CREATED="1560093661542" MODIFIED="1560093956967" MAX_WIDTH="240.0 cm"/>
+</node>
+<node TEXT="&#x4e8c;&#x5143;&#x51fd;&#x6570;&#x7684;n&#x9636;&#x5e26;&#x6709;Lagrange&#x578b;&#x4f59;&#x9879;&#x7684;&#x6cf0;&#x52d2;&#x516c;&#x5f0f;" ID="ID_1369078565" CREATED="1560088872136" MODIFIED="1560094042751">
+<node TEXT="\latex $f(x,y)=f(a,b)\\+[\frac\partial{\partial x}(x-a)+\frac\partial{\partial y}(y-b)]f(a,b)\\+\frac12[\frac\partial{\partial x}(x-a)+\frac\partial{\partial y}(y-b)]^2f(a,b)\\+\cdots\\+\frac1{n!}[\frac\partial{\partial x}(x-a)+\frac\partial{\partial y}(y-b)]^nf(a,b)\\+\frac1{(n+1)!}[\frac\partial{\partial x}(x-a)+\frac\partial{\partial y}(y-b)]^{n+1}f(a+\theta\Delta x,b+\theta\Delta y),\\ 0&lt;\theta&lt;1$" ID="ID_576984454" CREATED="1560094084394" MODIFIED="1560094220877"/>
+</node>
+</node>
+</node>
+</map>
