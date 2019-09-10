6adaa11550bb5d2dadb93d1c026a5080a4a85733
BatchLion <30330388+BatchLion@users.noreply.github.com>
2019-09-10 09:28:42 +0800

    Add files via upload

---
+++ 基础习题课讲义/Source Codes/29/Figures29/稳定性初步.mm
@@ -0,0 +1,149 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="&#x7a33;&#x5b9a;&#x6027;&#x521d;&#x6b65;" FOLDED="false" ID="ID_163487068" CREATED="1559399608810" MODIFIED="1563332599135" STYLE="oval" MAX_WIDTH="120.0 cm">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="72"/>
+<hook NAME="MapStyle" zoom="0.15">
+    <properties fit_to_viewport="false" show_icon_for_attributes="true" show_note_icons="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt" MAX_WIDTH="120.0 cm">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble" MAX_WIDTH="120.0 cm">
+<font SIZE="72"/>
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork" MAX_WIDTH="120.0 cm">
+<font NAME="SansSerif" SIZE="72" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" MAX_WIDTH="120.0 cm">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" MAX_WIDTH="120.0 cm">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT" MAX_WIDTH="120.0 cm">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" MAX_WIDTH="120.0 cm">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<font SIZE="72"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble" MAX_WIDTH="120.0 cm">
+<font SIZE="72"/>
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork" MAX_WIDTH="120.0 cm">
+<font NAME="Liberation Sans" SIZE="72" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork" MAX_WIDTH="120.0 cm">
+<font NAME="Liberation Sans" SIZE="72" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900" MAX_WIDTH="120.0 cm">
+<font NAME="Liberation Sans" SIZE="72" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" MAX_WIDTH="120.0 cm">
+<icon BUILTIN="yes"/>
+<font SIZE="72"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble" MAX_WIDTH="120.0 cm">
+<font SIZE="72"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt" MAX_WIDTH="120.0 cm">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff" MAX_WIDTH="120.0 cm">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439" MAX_WIDTH="120.0 cm">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000" MAX_WIDTH="120.0 cm">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111" MAX_WIDTH="120.0 cm">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" MAX_WIDTH="120.0 cm">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" MAX_WIDTH="120.0 cm">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" MAX_WIDTH="120.0 cm">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" MAX_WIDTH="120.0 cm">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" MAX_WIDTH="120.0 cm">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" MAX_WIDTH="120.0 cm">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" MAX_WIDTH="120.0 cm">
+<font SIZE="72"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="10" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="&#x7a33;&#x5b9a;&#x6027;&#x7684;&#x57fa;&#x672c;&#x6982;&#x5ff5;" POSITION="right" ID="ID_1653680933" CREATED="1563332570684" MODIFIED="1563332611116">
+<edge COLOR="#007c00"/>
+<node TEXT="\latex &#x4e00;&#x9636;&#x5fae;&#x5206;&#x65b9;&#x7a0b;&#x7ec4; $\frac{\mathrm d\mathbf\textit x}{\mathrm dt}=\mathbf\textit f(t,\mathbf\textit x)$," ID="ID_393460592" CREATED="1563332732446" MODIFIED="1563332789167">
+<node TEXT="\latex &#x5e73;&#x8861;&#x70b9;(&#x5947;&#x70b9;) $\mathbf\textit x^*,s.t.\mathbf\textit f(t,\mathbf\textit x)=\mathbf0$" ID="ID_1532864700" CREATED="1563332669863" MODIFIED="1563332864132"/>
+<node TEXT="\latex &#x5e73;&#x8861;&#x89e3; $\mathbf\textit x^*,s.t.\frac{\mathrm d\mathbf\textit x^*}{\mathrm dt}=\mathbf\textit f(t,\mathbf\textit x^*)$" ID="ID_278085330" CREATED="1563332684024" MODIFIED="1563332975002"/>
+</node>
+<node TEXT="&#x7a33;&#x5b9a;&#x6027;&#x7684;&#x5b9a;&#x4e49;" ID="ID_1235257972" CREATED="1563334068660" MODIFIED="1563334074736">
+<node TEXT="\latex &#x8bbe;$\mathbf\textit\varphi(t)$&#x662f;&#x4e00;&#x9636;&#x5fae;&#x5206;&#x65b9;&#x7a0b;&#x7ec4;$\frac{\mathrm d\mathbf\textit x}{\mathrm dt}=\mathbf\textit f(t,\mathbf\textit x)$&#x7684;&#x4e00;&#x4e2a;&#x89e3;&#xff0c;\\&#x82e5;$\forall t_0,\forall\varepsilon&gt;0,\exists\delta=\delta(t_0,\varepsilon),s.t$&#x5f53;$\|\mathbf\textit x(t_0),\mathbf\textit\varphi(t_0)\|&lt;\delta$&#x65f6;&#xff0c;\\$\|\mathbf\textit x(t)-\mathbf\textit\varphi(t)\|&lt;\varepsilon$&#x5bf9;&#x4efb;&#x610f;&#x7684;$t\geqslant t_0$&#x6210;&#x7acb;&#xff0c;\\&#x5219;&#x79f0;$\mathbf\textit\varphi(t)$&#x5728;&#x674e;&#x96c5;&#x666e;&#x8bfa;&#x592b;&#x610f;&#x4e49;&#x4e0b;&#x7a33;&#x5b9a;. &#x5426;&#x5219;&#x79f0;$\mathbf\textit\varphi(t)$&#x4e0d;&#x7a33;&#x5b9a;." ID="ID_902085664" CREATED="1563333046707" MODIFIED="1563334801037"/>
+</node>
+<node TEXT="&#x6e10;&#x8fdb;&#x7a33;&#x5b9a;&#x6027;" ID="ID_1840126199" CREATED="1563334091611" MODIFIED="1563334098651">
+<node TEXT="\latex &#x8bbe;$\mathbf\textit\varphi(t)$&#x662f;&#x4e00;&#x9636;&#x5fae;&#x5206;&#x65b9;&#x7a0b;&#x7ec4;$\frac{\mathrm d\mathbf\textit x}{\mathrm dt}=\mathbf\textit f(t,\mathbf\textit x)$&#x7684;&#x4e00;&#x4e2a;&#x89e3;&#xff0c;\\&#x82e5;$\forall t_0,\exists\eta&gt;0,s.t.$&#x5f53;$\|\mathbf\textit x(t_0)-\mathbf\textit\varphi(t_0)\|&lt;\eta$&#x65f6;, \\$\lim\limits_{t\rightarrow+\infty}\|\mathbf\textit x(t)-\mathbf\textit\varphi(t)\|=0$, &#x5219;$\mathbf\textit\varphi(t)$&#x6e10;&#x8fdb;&#x7a33;&#x5b9a;. \\&#x5f53;$\eta=+\infty$&#x65f6;, &#x5373;&#x65b9;&#x7a0b;&#x7ec4;$\frac{\mathrm d\mathbf\textit x}{\mathrm dt}=\mathbf\textit f(t,\mathbf\textit x)$&#x7684;&#x4efb;&#x610f;&#x89e3;$\mathbf\textit x(t)$&#x5747;&#x6ee1;&#x8db3;\\$\lim\limits_{t\rightarrow+\infty}\|\mathbf\textit x(t)-\mathbf\textit\varphi(t)\|=0$, &#x5219;$\mathbf\textit\varphi(t)$&#x5168;&#x5c40;&#x6e10;&#x8fdb;&#x7a33;&#x5b9a;." ID="ID_1199730570" CREATED="1563334464034" MODIFIED="1563334805965"/>
+</node>
+<node TEXT="\latex &#x4f8b;&#xff1a;$\varphi(t)\equiv0$&#x662f;&#x5fae;&#x5206;&#x65b9;&#x7a0b;$\frac{\mathrm dx}{\mathrm dt}=cx$&#x7684;&#x5e73;&#x8861;&#x89e3;, \\$x(t)=x_0\mathrm e^{c(t-t_0)}$&#x662f;&#x8be5;&#x65b9;&#x7a0b;&#x7684;&#x4efb;&#x610f;&#x89e3;&#xff0c;&#x5219;" ID="ID_1852404162" CREATED="1563334839597" MODIFIED="1563335682992">
+<node TEXT="\latex (1)&#x5f53;$c&gt;0$&#x65f6;&#xff0c;$\forall\delta&gt;0$&#x5f53;$\|x(t_0)-0\|=|x_0|&lt;\delta$&#x65f6;\\&#x603b;&#x5b58;&#x5728;$t_1&gt;t_0,s.t.\|x(t_1)-0\|=|x_0|\mathrm e^{c(t_1-t_0)}&gt;1=\varepsilon_0$&#xff0c; &#x6545;&#x5e73;&#x8861;&#x89e3;$\varphi(t)\equiv0$&#x4e0d;&#x7a33;&#x5b9a;&#xff1b;" ID="ID_666776097" CREATED="1563334859186" MODIFIED="1563335959941"/>
+<node TEXT="\latex (2)&#x5f53;$c=0$&#x65f6;&#xff0c;$x(t)=x_0$, $\forall t_0,\forall\epsilon&gt;0$, &#x53d6;$\delta=\varepsilon$, \\&#x5219;&#x5f53;$\|x(t_0)-0\|=|x_0|&lt;\delta$&#x65f6;&#xff0c;$\|x(t)-0\|=|x_0|&lt;\varepsilon$&#x5bf9;&#x4e8e;&#x4efb;&#x610f;&#x7684;$t&gt;t_0$&#x90fd;&#x6210;&#x7acb;&#xff0c;\\&#x6545;&#x5e73;&#x8861;&#x89e3;$\varphi(t)\equiv0$&#x7a33;&#x5b9a;&#xff1b;\\&#x4f46;$\lim\limits_{t\rightarrow+\infty}x(t)=x_0\neq0$&#x6545;&#x5e73;&#x8861;&#x89e3;$\varphi(t)\equiv0$&#x4e0d;&#x6e10;&#x8fdb;&#x7a33;&#x5b9a;." ID="ID_786338324" CREATED="1563335208658" MODIFIED="1563336007447"/>
+<node TEXT="\latex (3)&#x5f53;$c&lt;0$&#x65f6;&#xff0c;$x(t)=x_0\mathrm e^{c(t-t_0)}$, $\forall t_0,\forall\epsilon&gt;0$, &#x53d6;$\delta=\varepsilon$, \\&#x5219;&#x5f53;$\|x(t_0)-0\|=|x_0|&lt;\delta$&#x65f6;&#xff0c;$\|x(t)-0\|=|x_0|\mathrm e^{c(t-t_0)}&lt;\varepsilon$&#x5bf9;&#x4e8e;&#x4efb;&#x610f;&#x7684;$t&gt;t_0$&#x90fd;&#x6210;&#x7acb;&#xff0c;\\&#x6545;&#x5e73;&#x8861;&#x89e3;$\varphi(t)\equiv0$&#x7a33;&#x5b9a;&#xff1b;\\&#x56e0;$\lim\limits_{t\rightarrow+\infty}x(t)=\lim\limits_{t\rightarrow+\infty}x_0\mathrm e^{c(t-t_0)}=0$&#x6545;&#x5e73;&#x8861;&#x89e3;$\varphi(t)\equiv0$&#x5168;&#x5c40;&#x6e10;&#x8fdb;&#x7a33;&#x5b9a;." ID="ID_311505224" CREATED="1563335207701" MODIFIED="1563336117119"/>
+</node>
+</node>
+<node TEXT="\latex &#x7ebf;&#x6027;&#x5fae;&#x5206;&#x65b9;&#x7a0b;&#x7ec4;$\frac{\mathrm d\mathbf\textit x}{\mathrm dt}=\mathbf\textit A(t)\mathbf\textit x+\mathbf\textit f(t)$&#x89e3;&#x7684;&#x7a33;&#x5b9a;&#x6027;" POSITION="right" ID="ID_1305430976" CREATED="1563332611329" MODIFIED="1563416546427">
+<edge COLOR="#7c007c"/>
+<node TEXT="\latex &#x53ef;&#x5c06;&#x975e;&#x9f50;&#x6b21;&#x65b9;&#x7a0b;&#x7ec4;&#x7684;&#x4efb;&#x610f;&#x89e3;$\mathbf\textit\varphi(t)$&#x7684;&#x7a33;&#x5b9a;&#x6027;&#x95ee;&#x9898;&#x8f6c;&#x5316;&#x4e3a;&#x9f50;&#x6b21;&#x65b9;&#x7a0b;&#x7ec4;$\frac{\mathrm d\mathbf\textit y}{\mathrm dt}=\mathbf\textit A(t)\mathbf\textit y$&#x96f6;&#x89e3;&#x7684;&#x7a33;&#x5b9a;&#x6027;&#x95ee;&#x9898;" ID="ID_1323656091" CREATED="1563416071988" MODIFIED="1563417777620">
+<node TEXT="&#x7ebf;&#x6027;&#x5fae;&#x5206;&#x65b9;&#x7a0b;&#x7ec4;&#x7684;&#x6240;&#x6709;&#x89e3;&#x5177;&#x6709;&#x76f8;&#x540c;&#x7684;&#x7a33;&#x5b9a;&#x6027;&#xff0c;&#x4e0e;&#x8be5;&#x65b9;&#x7a0b;&#x7ec4;&#x5bf9;&#x5e94;&#x7684;&#x9f50;&#x6b21;&#x65b9;&#x7a0b;&#x7ec4;&#x96f6;&#x89e3;&#x7684;&#x7a33;&#x5b9a;&#x6027;&#x76f8;&#x540c;&#xff0c;&#x6545;&#x7ebf;&#x6027;&#x5fae;&#x5206;&#x65b9;&#x7a0b;&#x7ec4;&#x89e3;&#x7684;&#x7a33;&#x5b9a;&#x6027;&#x4e5f;&#x662f;&#x8be5;&#x7ebf;&#x6027;&#x65b9;&#x7a0b;&#x7ec4;&#x7684;&#x7a33;&#x5b9a;&#x6027;" ID="ID_608188850" CREATED="1563417208743" MODIFIED="1563417777617" HGAP_QUANTITY="-2667.2499200925254 pt" VSHIFT_QUANTITY="206.99999383091946 pt"/>
+</node>
+<node TEXT="\latex &#x7ebf;&#x6027;&#x5e38;&#x7cfb;&#x6570;&#x975e;&#x9f50;&#x6b21;&#x65b9;&#x7a0b;&#x7ec4;\\$\frac{\mathrm d\mathbf\textit x}{\mathrm dt}=\mathbf\textit A\mathbf\textit x+\mathbf\textit f(t)$&#x7684;&#x4efb;&#x610f;&#x89e3;$\mathbf\textit\varphi(t)$&#x7684;&#x7a33;&#x5b9a;&#x6027;&#x5224;&#x5b9a;" ID="ID_1825035527" CREATED="1563416269412" MODIFIED="1563416610611">
+<node TEXT="\latex &#x5e38;&#x6570;&#x77e9;&#x9635;$\mathbf\textit A$&#x7684;&#x7279;&#x5f81;&#x503c;&#x4e3a;$\lambda_1,\lambda_2,\cdots,\lambda_k$, &#x5219;" ID="ID_1838990941" CREATED="1563416601719" MODIFIED="1563417694660">
+<node TEXT="\latex (1)&#x82e5;$\mathbf\textit A$&#x5b58;&#x5728;&#x6b63;&#x5b9e;&#x90e8;&#x7684;&#x7279;&#x5f81;&#x503c;&#xff0c;&#x5219;&#x8be5;&#x7ebf;&#x6027;&#x65b9;&#x7a0b;&#x7ec4;&#x4e0d;&#x7a33;&#x5b9a;" ID="ID_1956491717" CREATED="1563416674827" MODIFIED="1563417694660" HGAP_QUANTITY="-1155.2499651536357 pt" VSHIFT_QUANTITY="314.2499906346205 pt"/>
+<node TEXT="\latex (2)&#x82e5;$\mathrm{Re}(\lambda_i)\leqslant0(i=1,2,\cdots,k)$&#xff0c; &#x4e14;&#x5f53;&#x6709;&#x96f6;&#x7279;&#x5f81;&#x503c;&#x65f6;&#xff0c; &#x96f6;&#x7279;&#x5f81;&#x503c;&#x4e0d;&#x662f;&#x91cd;&#x7279;&#x5f81;&#x503c;&#xff0c;\\&#x5219;&#x8be5;&#x7ebf;&#x6027;&#x65b9;&#x7a0b;&#x7ec4;&#x7a33;&#x5b9a;" ID="ID_564270393" CREATED="1563416816450" MODIFIED="1563417687319" HGAP_QUANTITY="-1155.2499651536357 pt" VSHIFT_QUANTITY="4.499999865889553 pt"/>
+<node TEXT="\latex (3)&#x82e5;$\mathrm{Re}(\lambda_i)&lt;0(i=1,2,\cdots,k)$, &#x5219;&#x8be5;&#x7ebf;&#x6027;&#x65b9;&#x7a0b;&#x7ec4;&#x6e10;&#x8fdb;&#x7a33;&#x5b9a;" ID="ID_187711490" CREATED="1563417023759" MODIFIED="1563417672016" HGAP_QUANTITY="-1150.7499652877461 pt" VSHIFT_QUANTITY="-9.749999709427366 pt"/>
+</node>
+</node>
+<node TEXT="&#x7ebf;&#x6027;&#x5e38;&#x7cfb;&#x6570;&#x975e;&#x9f50;&#x6b21;&#x65b9;&#x7a0b;&#x7ec4;&#x7684;&#x7a33;&#x5b9a;&#x6027;&#x5224;&#x5b9a;&#x6b65;&#x9aa4;" ID="ID_1466051458" CREATED="1563417400328" MODIFIED="1563417442435">
+<node TEXT="\latex &#x7b2c;&#x4e00;&#x6b65;&#xff1a; &#x6c42;&#x7cfb;&#x6570;&#x77e9;&#x9635;$\mathbf\textit A$&#x7684;&#x7279;&#x5f81;&#x503c;$\lambda_1,\lambda_2,\cdots,\lambda_k$&#xff1b;" ID="ID_1385119768" CREATED="1563417442441" MODIFIED="1563417550949"/>
+<node TEXT="\latex &#x7b2c;&#x4e8c;&#x6b65;&#xff1a; &#x6839;&#x636e;&#x4e0a;&#x8ff0;&#x5224;&#x5b9a;&#x5b9a;&#x7406;&#x5224;&#x5b9a;&#x8be5;&#x7ebf;&#x6027;&#x65b9;&#x7a0b;&#x7ec4;&#x7684;&#x7a33;&#x5b9a;&#x6027;." ID="ID_96054890" CREATED="1563417476970" MODIFIED="1563419282422"/>
+</node>
+</node>
+<node TEXT="&#x975e;&#x7ebf;&#x6027;&#x5fae;&#x5206;&#x65b9;&#x7a0b;&#x7ec4;&#x89e3;&#x7684;&#x7a33;&#x5b9a;&#x6027;&#x7684;&#x5224;&#x5b9a;" POSITION="right" ID="ID_1988350692" CREATED="1563332636308" MODIFIED="1563332656341">
+<edge COLOR="#007c7c"/>
+<node TEXT="&#x81ea;&#x6cbb;&#x7cfb;&#x7edf;&#xff1a;" ID="ID_1911994522" CREATED="1563417877983" MODIFIED="1563417890835">
+<node TEXT="\latex $\begin{cases}\frac{\mathrm dx_1}{\mathrm dt}=f_1(x_1,x_2,\cdots,x_n)\\\frac{\mathrm dx_2}{\mathrm dt}=f_2(x_1,x_2,\cdots,x_n)\\\vdots\\\frac{\mathrm dx_n}{\mathrm dt}=f_n(x_1,x_2,\cdots,x_n)\end{cases}$, $\mathbf\textit\xi$&#x4e3a;&#x5176;&#x4e00;&#x4e2a;&#x5e73;&#x8861;&#x89e3;" ID="ID_130256081" CREATED="1563417893111" MODIFIED="1563418219452"/>
+</node>
+<node TEXT="\latex &#x81ea;&#x6cbb;&#x7cfb;&#x7edf;&#x5728;&#x5e73;&#x8861;&#x89e3;$\mathbf\textit\xi$&#x5904;&#x7684;&#x7ebf;&#x6027;&#x8fd1;&#x4f3c;&#x65b9;&#x7a0b;&#xff1a;" ID="ID_877556805" CREATED="1563418022433" MODIFIED="1563418454821">
+<node TEXT="\latex $\frac{\mathrm d\mathbf\textit y}{\mathrm dt}=\mathbf\textit A\mathbf\textit y$" ID="ID_1708468061" CREATED="1563418042346" MODIFIED="1563418366438">
+<node TEXT="\latex $\mathbf\textit A=\frac{\partial(f_1,f_2,\cdots,f_n)}{\partial(x_1,x_2,\cdots,x_n)}\Big|_{\mathbf\textit\xi}$&#x4e3a;&#x5e73;&#x8861;&#x89e3;$\mathbf\textit\xi$&#x5904;&#x7684;&#x96c5;&#x53ef;&#x6bd4;&#x77e9;&#x9635;" ID="ID_1290334346" CREATED="1563418119445" MODIFIED="1563418366436" HGAP_QUANTITY="-630.2499807998544 pt" VSHIFT_QUANTITY="139.49999584257614 pt"/>
+</node>
+</node>
+<node TEXT="\latex &#x81ea;&#x6cbb;&#x7cfb;&#x7edf;&#x5e73;&#x8861;&#x89e3;$\mathbf\textit\xi$&#x7a33;&#x5b9a;&#x6027;&#x7684;&#x5224;&#x5b9a;&#x5b9a;&#x7406;" ID="ID_616226742" CREATED="1563418411048" MODIFIED="1563418484177">
+<node TEXT="\latex (1)&#x5f53;$\mathbf\textit A$&#x7684;&#x6240;&#x6709;&#x7279;&#x5f81;&#x503c;&#x7684;&#x5b9e;&#x90e8;&#x90fd;&#x662f;&#x8d1f;&#x6570;&#x65f6;&#xff0c;&#x5e73;&#x8861;&#x89e3;$\mathbf\textit\xi$&#x6e10;&#x8fdb;&#x7a33;&#x5b9a;" ID="ID_777437192" CREATED="1563418485532" MODIFIED="1563418854882"/>
+<node TEXT="\latex (2)&#x5f53;$\mathbf\textit A$&#x7684;&#x7279;&#x5f81;&#x503c;&#x4e2d;&#x81f3;&#x5c11;&#x6709;&#x4e00;&#x4e2a;&#x5177;&#x6709;&#x6b63;&#x5b9e;&#x90e8;&#x65f6;&#xff0c;&#x5e73;&#x8861;&#x89e3;$\mathbf\textit\xi$&#x4e0d;&#x7a33;&#x5b9a;" ID="ID_684278078" CREATED="1563418558458" MODIFIED="1563418878075"/>
+<node TEXT="\latex (3)&#x5f53;$\mathbf\textit A$&#x7684;&#x6240;&#x6709;&#x7279;&#x5f81;&#x503c;&#x7684;&#x5b9e;&#x90e8;&#x90fd;&#x975e;&#x6b63;&#x4f46;&#x6709;&#x96f6;&#x5b9e;&#x90e8;&#x7279;&#x5f81;&#x503c;&#x65f6;&#xff0c;\\&#x5e73;&#x8861;&#x89e3;$\mathbf\textit\xi$&#x7684;&#x7a33;&#x5b9a;&#x6027;&#x4e0d;&#x80fd;&#x7531;&#x5176;&#x7ebf;&#x6027;&#x8fd1;&#x4f3c;&#x65b9;&#x7a0b;&#x7ec4;&#x5224;&#x5b9a;" ID="ID_794487863" CREATED="1563418722752" MODIFIED="1563418902866"/>
+</node>
+<node TEXT="\latex &#x81ea;&#x6cbb;&#x7cfb;&#x7edf;&#x5e73;&#x8861;&#x89e3;$\mathbf\textit\xi$&#x7a33;&#x5b9a;&#x6027;&#x7684;&#x5224;&#x5b9a;&#x6b65;&#x9aa4;" ID="ID_446515983" CREATED="1563418943406" MODIFIED="1563418971739">
+<node TEXT="\latex &#x7b2c;&#x4e00;&#x6b65;&#xff1a;&#x4ee4;$\mathbf\textit f(x_1,\cdots,x_n)=\mathbf0$, &#x6c42;&#x51fa;&#x8be5;&#x81ea;&#x6cbb;&#x7cfb;&#x7edf;&#x7684;&#x6240;&#x6709;&#x5e73;&#x8861;&#x89e3;$\mathbf\textit\xi_i$;" ID="ID_1285100273" CREATED="1563418973934" MODIFIED="1563419122369"/>
+<node TEXT="\latex &#x7b2c;&#x4e8c;&#x6b65;&#xff1a;&#x6c42;&#x51fa;&#x5e73;&#x8861;&#x89e3;$\mathbf\textit\xi_i$&#x5904;&#x7684;&#x96c5;&#x53ef;&#x6bd4;&#x77e9;&#x9635;$\mathbf\textit A$;" ID="ID_691961779" CREATED="1563419063776" MODIFIED="1563419230625"/>
+<node TEXT="\latex &#x7b2c;&#x4e09;&#x6b65;&#xff1a;&#x6c42;&#x51fa;&#x5e73;&#x8861;&#x89e3;$\mathbf\textit\xi_i$&#x5904;&#x7684;&#x96c5;&#x53ef;&#x6bd4;&#x77e9;&#x9635;$\mathbf\textit A$&#x7684;&#x7279;&#x5f81;&#x503c;$\lambda_1,\cdots,\lambda_n$;" ID="ID_1913457933" CREATED="1563419132641" MODIFIED="1563419236221"/>
+<node TEXT="\latex &#x7b2c;&#x56db;&#x6b65;&#xff1a;&#x6839;&#x636e;&#x4e0a;&#x8ff0;&#x5224;&#x5b9a;&#x5b9a;&#x7406;&#x5224;&#x5b9a;&#x8be5;&#x81ea;&#x6cbb;&#x7cfb;&#x7edf;&#x7684;&#x7a33;&#x5b9a;&#x6027;." ID="ID_1100517761" CREATED="1563419248144" MODIFIED="1563419268837"/>
+</node>
+</node>
+</node>
+</map>
