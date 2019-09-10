cf1d4c484780981c6492bf404a6e88298cb1f751
BatchLion <30330388+BatchLion@users.noreply.github.com>
2019-09-10 09:50:46 +0800

    Add files via upload

---
+++ 基础习题课期末复习/Source Codes/20190607/多元函数微分学.mm
@@ -0,0 +1,166 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="&#x591a;&#x5143;&#x51fd;&#x6570;&#x5fae;&#x5206;&#x5b66;" FOLDED="false" ID="ID_962889671" CREATED="1559979024597" MODIFIED="1559980102884" STYLE="oval" MAX_WIDTH="120.0 cm">
+<font SIZE="72"/>
+<hook NAME="MapStyle" zoom="0.15">
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<font SIZE="72"/>
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork" MAX_WIDTH="120.0 cm">
+<font NAME="SansSerif" SIZE="72" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<font SIZE="72"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<font SIZE="72"/>
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="72" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="72" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="72" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+<font SIZE="72"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<font SIZE="72"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<font SIZE="72"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<font SIZE="72"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="8" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="&#x591a;&#x5143;&#x8fde;&#x7eed;&#x51fd;&#x6570;" POSITION="right" ID="ID_782259497" CREATED="1559895481415" MODIFIED="1559979164334" MAX_WIDTH="120.0 cm">
+<font SIZE="72" BOLD="false"/>
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="&#x504f;&#x5bfc;&#x6570;" POSITION="right" ID="ID_1455304743" CREATED="1559895486883" MODIFIED="1559979182812" MAX_WIDTH="120.0 cm">
+<font SIZE="72" BOLD="false"/>
+<edge COLOR="#00ff00"/>
+<node TEXT="&#x504f;&#x5bfc;&#x6570;&#x7684;&#x6982;&#x5ff5;" ID="ID_964607859" CREATED="1559896043230" MODIFIED="1559896052543">
+<node TEXT="&#x504f;&#x5bfc;&#x6570;&#x7684;&#x5b9a;&#x4e49;" ID="ID_1808687389" CREATED="1559895829000" MODIFIED="1559895853892">
+<font SIZE="72"/>
+<node TEXT="\latex &#x5b9e;&#x8d28;&#x662f;&#x4e00;&#x5143;&#x51fd;&#x6570;&#x7684;&#x5bfc;&#x6570;&#xff1a; $\frac{\partial f(a,b)}{\partial x}=\frac{\mathrm df(x,b)}{\mathrm dx}\big|_{x=a}$" ID="ID_937992180" CREATED="1559917704699" MODIFIED="1559917842489"/>
+<node TEXT="&#x504f;&#x5bfc;&#x6570;&#x5b58;&#x5728;&#x4e0e;&#x51fd;&#x6570;&#x5728;&#x8be5;&#x70b9;&#x8fde;&#x7eed;&#x6ca1;&#x6709;&#x5fc5;&#x7136;&#x7684;&#x8054;&#x7cfb;" ID="ID_1931128074" CREATED="1559917717828" MODIFIED="1559920795343"/>
+</node>
+<node TEXT="&#x4e8c;&#x5143;&#x51fd;&#x6570;&#x504f;&#x5bfc;&#x6570;&#x7684;&#x51e0;&#x4f55;&#x610f;&#x4e49;" ID="ID_1131099641" CREATED="1559895959725" MODIFIED="1559896004143"/>
+</node>
+<node TEXT="&#x9ad8;&#x9636;&#x504f;&#x5bfc;&#x6570;" ID="ID_424157983" CREATED="1559896058765" MODIFIED="1559896073231">
+<node TEXT="&#x6982;&#x5ff5;" ID="ID_560074531" CREATED="1559896088060" MODIFIED="1559896126485"/>
+<node TEXT="&#x6df7;&#x5408;&#x504f;&#x5bfc;&#x6570;&#x4e0e;&#x6c42;&#x5bfc;&#x987a;&#x5e8f;&#x65e0;&#x5173;&#x7684;&#x5145;&#x5206;&#x6761;&#x4ef6;" ID="ID_1845857041" CREATED="1559896134052" MODIFIED="1559896144065"/>
+</node>
+</node>
+<node TEXT="&#x5168;&#x5fae;&#x5206;" POSITION="right" ID="ID_1549676821" CREATED="1559895502313" MODIFIED="1559979213937" MAX_WIDTH="120.0 cm">
+<font SIZE="72" BOLD="false"/>
+<edge COLOR="#00007c"/>
+<node TEXT="&#x5168;&#x5fae;&#x5206;&#x7684;&#x6982;&#x5ff5;" ID="ID_1223131758" CREATED="1559896177033" MODIFIED="1559896187266">
+<node TEXT="\latex &#x5b9a;&#x4e49;&#xff1a; $\exists$ &#x5e38;&#x6570; $A,B,s.t. \Delta f(a,b)=f(a+\Delta x,b+\Delta y)-f(a,b)=A\Delta x+B\Delta y+o(\sqrt{x^2+y^2})$" ID="ID_1239898100" CREATED="1559909600183" MODIFIED="1559921859007"/>
+<node TEXT="\latex &#x53ef;&#x5fae;$\Rightarrow$&#x8fde;&#x7eed;" ID="ID_1250513203" CREATED="1559909620283" MODIFIED="1559909827892"/>
+<node TEXT="\latex &#x51e0;&#x4f55;&#x610f;&#x4e49;&#x2014;&#x2014;&#x4ee5;&#x5e73;&#x9762;&#x4ee3;&#x66ff;&#x66f2;&#x9762; $f(x,y)=f(a,b)+A(x-a)+B(y-b)$" ID="ID_316649990" CREATED="1559909663929" MODIFIED="1559921920592"/>
+</node>
+<node TEXT="&#x53ef;&#x5fae;&#x7684;&#x5fc5;&#x8981;&#x6761;&#x4ef6;&#x4e0e;&#x5145;&#x5206;&#x6761;&#x4ef6;" ID="ID_360089611" CREATED="1559896187787" MODIFIED="1559896201189">
+<node TEXT="&#x53ef;&#x5fae;&#x7684;&#x5fc5;&#x8981;&#x6761;&#x4ef6;" ID="ID_1183094233" CREATED="1559896217375" MODIFIED="1559959412665">
+<node TEXT="\latex &#x53ef;&#x5fae;$\Rightarrow$&#x53ef;&#x504f;&#x5bfc;, &#x4e14;$\mathrm df(a,b)=\frac{\partial f}{\partial x}\mathrm dx+\frac{\partial f}{\partial y}\mathrm dy$" ID="ID_415172749" CREATED="1559910362509" MODIFIED="1559959412659" HGAP_QUANTITY="83.74999792128807 pt" VSHIFT_QUANTITY="59.249998234212455 pt"/>
+</node>
+<node TEXT="&#x53ef;&#x5fae;&#x7684;&#x4e00;&#x4e2a;&#x5145;&#x5206;&#x6761;&#x4ef6;" ID="ID_1900908665" CREATED="1559896237202" MODIFIED="1559959406385">
+<node TEXT="\latex &#x5145;&#x5206;&#x6761;&#x4ef6;&#x5b9a;&#x7406;" ID="ID_808697518" CREATED="1559959107865" MODIFIED="1559959402437" HGAP_QUANTITY="92.74999765306717 pt" VSHIFT_QUANTITY="-35.99999892711642 pt"/>
+<node TEXT="\latex $f(x,y)$&#x5728;&#x533a;&#x57df;$D$&#x4e0a;&#x4e00;&#x9636;&#x504f;&#x5bfc;&#x6570;&#x8fde;&#x7eed; (&#x5373;$f(x,y)\in C^1(D)$) $\Rightarrow f(x,y)$&#x5728;$D$&#x4e0a;&#x53ef;&#x5fae;" ID="ID_1074251577" CREATED="1559921940658" MODIFIED="1559961091737" HGAP_QUANTITY="112.99999704957018 pt" VSHIFT_QUANTITY="49.49999852478509 pt" MAX_WIDTH="60.0 cm"/>
+</node>
+<node TEXT="&#x53ef;&#x5fae;&#x7684;&#x5145;&#x5206;&#x5fc5;&#x8981;&#x6761;&#x4ef6;" ID="ID_1273435115" CREATED="1559896268476" MODIFIED="1559959345468">
+<node TEXT="&#x7406;&#x8bba;&#x610f;&#x4e49;" ID="ID_1461046070" CREATED="1559922312645" MODIFIED="1559959345465" HGAP_QUANTITY="114.49999700486669 pt" VSHIFT_QUANTITY="68.99999794363981 pt"/>
+</node>
+<node TEXT="&#x591a;&#x5143;&#x5fae;&#x5206;&#x5f62;&#x5f0f;&#x7684;&#x539f;&#x51fd;&#x6570;" ID="ID_791894313" CREATED="1559896277627" MODIFIED="1559959341759">
+<node TEXT="&#x4e0d;&#x5b9a;&#x79ef;&#x5206;&#x6cd5;&#x6c42;&#x539f;&#x51fd;&#x6570;" ID="ID_98904661" CREATED="1559922323958" MODIFIED="1559959341756" HGAP_QUANTITY="88.99999776482589 pt" VSHIFT_QUANTITY="53.999998390674634 pt"/>
+</node>
+</node>
+</node>
+<node TEXT="&#x590d;&#x5408;&#x51fd;&#x6570;&#x5fae;&#x5206;&#x6cd5;" POSITION="right" ID="ID_177738321" CREATED="1559895505993" MODIFIED="1559979232470" MAX_WIDTH="120.0 cm">
+<font SIZE="72" BOLD="false"/>
+<edge COLOR="#007c00"/>
+<node TEXT="&#x590d;&#x5408;&#x51fd;&#x6570;&#x7684;&#x5168;&#x5fae;&#x5206;&#x4e0e;&#x504f;&#x5bfc;&#x6570;" ID="ID_1426332537" CREATED="1559896332673" MODIFIED="1559964590398">
+<node TEXT="\latex $z=f(u(x,y),v(x,y))$ &#x7684;&#x504f;&#x5bfc;&#x6570; $\begin{cases}\frac{\partial z}{\partial x}=\frac{\partial f}{\partial u}\frac{\partial u}{\partial x}+\frac{\partial f}{\partial v}\frac{\partial v}{\partial x},\\ \frac{\partial z}{\partial y}=\frac{\partial f}{\partial u}\frac{\partial u}{\partial y}+\frac{\partial f}{\partial v}\frac{\partial v}{\partial y}\end{cases}$" ID="ID_1682564978" CREATED="1559922459896" MODIFIED="1559964693004" HGAP_QUANTITY="-10.749999262392542 pt" VSHIFT_QUANTITY="110.24999671429406 pt">
+<hook URI="Figures20190607/ChainRule.bmp" SIZE="0.36927998" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+<node TEXT="&#x9690;&#x51fd;&#x6570;&#x5fae;&#x5206;&#x6cd5;" POSITION="right" ID="ID_324720766" CREATED="1559895524058" MODIFIED="1559979204723" MAX_WIDTH="120.0 cm">
+<font SIZE="72" BOLD="false"/>
+<edge COLOR="#7c0000"/>
+<node TEXT="&#x9690;&#x51fd;&#x6570;&#x7684;&#x6982;&#x5ff5;" ID="ID_399049662" CREATED="1559896400496" MODIFIED="1559896422499"/>
+<node TEXT="&#x9690;&#x51fd;&#x6570;&#x7684;&#x5b58;&#x5728;&#x6027;&#x4e0e;&#x53ef;&#x5bfc;&#x6027;" ID="ID_1435469584" CREATED="1559896422654" MODIFIED="1559961560491">
+<node TEXT="\latex $F(x,y,z)=0$ &#x4e24;&#x8fb9;&#x5fae;&#x5206; $F_x\mathrm dx+F_y\mathrm dy+F_z\mathrm dz=0$ $\Rightarrow$ $\mathrm dz=-\frac{F_x}{F_z}\mathrm dx-\frac{F_y}{F_z}\mathrm dy=\frac{\partial z}{\partial x}\mathrm dx+\frac{\partial z}{\partial y}\mathrm dy$" ID="ID_1111469923" CREATED="1559922548678" MODIFIED="1560005219200" HGAP_QUANTITY="78.49999807775026 pt" VSHIFT_QUANTITY="-50.24999850243333 pt" MAX_WIDTH="58.0 cm"/>
+<node TEXT="\latex $F(x,y,z)=0$ &#x4e24;&#x8fb9;&#x6c42;&#x5173;&#x4e8e;$x,y$&#x7684;&#x504f;&#x5bfc;&#x6570; $\begin{cases}F_x+F_z\frac{\partial z}{\partial x}=0,\\ F_y+F_z\frac{\partial z}{\partial y}=0,\end{cases}$ &#x89e3;&#x51fa;$\frac{\partial z}{\partial x},\frac{\partial z}{\partial y}$" ID="ID_1411261843" CREATED="1559961377390" MODIFIED="1559961560485" HGAP_QUANTITY="83.74999792128807 pt" VSHIFT_QUANTITY="-44.999998658895535 pt"/>
+</node>
+<node TEXT="&#x9690;&#x51fd;&#x6570;&#x7ec4;&#x7684;&#x5fae;&#x5206;&#x6cd5;" ID="ID_1659856907" CREATED="1559896431076" MODIFIED="1559962413760">
+<node TEXT="\latex $\begin{cases} F(x,y,z)=0,\\ G(x,y,z)=0,\end{cases}$ &#x5206;&#x522b;&#x5bf9;&#x4e24;&#x8fb9;&#x5fae;&#x5206; $\begin{cases} F_x\mathrm dx+F_y\mathrm dy+F_z\mathrm dz=0,\\ G_x\mathrm dx+G_y\mathrm dy+G_z\mathrm dz=0,\end{cases}$ $\Rightarrow$ $\begin{cases}F_x+F_y\frac{\mathrm dy}{\mathrm dx}+F_z\frac{\mathrm dz}{\mathrm dx}=0,\\ G_x+G_y\frac{\mathrm dy}{\mathrm dx}+G_z\frac{\mathrm dz}{\mathrm dx}=0,\end{cases}$ &#x89e3;&#x8be5;&#x7ebf;&#x6027;&#x65b9;&#x7a0b;&#x7ec4;&#x5f97; $\frac{\mathrm dy}{\mathrm dx},\frac{\mathrm dz}{\mathrm dx}$" ID="ID_1527786416" CREATED="1559922802505" MODIFIED="1560005267143" MAX_WIDTH="72.0 cm" HGAP_QUANTITY="58.24999868124728 pt" VSHIFT_QUANTITY="296.9999911487105 pt"/>
+<node TEXT="\latex $\begin{cases} F(x,y,z)=0,\\ G(x,y,z)=0,\end{cases}$ &#x5206;&#x522b;&#x5bf9;&#x4e24;&#x8fb9;&#x6c42;&#x5173;&#x4e8e;$x$&#x7684;&#x5bfc;&#x6570; $\begin{cases} F_x+F_y\frac{\mathrm dy}{\mathrm dx}+F_z\frac{\mathrm dz}{\mathrm dx}=0,\\ G_x+G_y\frac{\mathrm dy}{\mathrm dx}+G_z\frac{\mathrm dz}{\mathrm dx}=0,\end{cases}$ &#x89e3;&#x8be5;&#x7ebf;&#x6027;&#x65b9;&#x7a0b;&#x7ec4;&#x5f97;$\frac{\mathrm dy}{\mathrm dx},\frac{\mathrm dz}{\mathrm dx}$" ID="ID_559841999" CREATED="1559961595371" MODIFIED="1560005356271" HGAP_QUANTITY="49.249998949468164 pt" VSHIFT_QUANTITY="80.2499976083637 pt" MAX_WIDTH="60.0 cm"/>
+</node>
+</node>
+<node TEXT="&#x65b9;&#x5411;&#x5bfc;&#x6570;&#x4e0e;&#x68af;&#x5ea6;" POSITION="right" ID="ID_1503289865" CREATED="1559895651055" MODIFIED="1559979193787" HGAP_QUANTITY="15.499999955296518 pt" VSHIFT_QUANTITY="1.4999999552965178 pt" MAX_WIDTH="120.0 cm">
+<font SIZE="72"/>
+<edge COLOR="#00ffff"/>
+</node>
+<node TEXT="&#x591a;&#x5143;&#x51fd;&#x6570;&#x7684;&#x6cf0;&#x52d2;&#x516c;&#x5f0f;" POSITION="right" ID="ID_681771761" CREATED="1559895697160" MODIFIED="1559979190338" MAX_WIDTH="120.0 cm">
+<font SIZE="72"/>
+<edge COLOR="#ff00ff"/>
+</node>
+</node>
+</map>
